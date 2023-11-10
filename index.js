7d298ad4939c596d84902197a48f990f16f271b4
Matheus F. <matheusfreitas.a09@gmail.com>
2023-11-10 18:24:06 -0300

    recebendo parâmetros na rota (engole essa Márcio)

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express")
+const { constants } = require("os")
 const app = express()
 const porta = 3000
 const path = require("path")
 
 const caminhoBase = path.join(__dirname, "templates")
 
-app.get('/' , (requisicao, resposta) => (
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+app.get('/' , (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
-))
+})
 
 
 
-app.listen(porta, () => (
+app.listen(porta, () => {
     console.log(`Rodando na porta ${porta}`)
-))
+})
